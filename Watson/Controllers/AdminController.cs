09a9294d8b7bfbee2b878ad9c881d0710948d139
vpape <38445312+vpape@users.noreply.github.com>
2018-09-29 16:31:34 -0500

    Created new project

--- Watson/Controllers/AdminController.cs
+++ Watson/Controllers/AdminController.cs
@@ -8,9 +8,12 @@ using Watson.Models;
 
 namespace Watson.Controllers
 {
+    /// <summary>
+    /// This is where I give you all the information about my employees
+    /// </summary>
     public class AdminController : ApiController
     {
-
+        private WatsonTruckEntities db = new WatsonTruckEntities();
 
         List<Employee> employee = new List<Employee>();
 
@@ -19,6 +22,26 @@ namespace Watson.Controllers
             employee.Add(new Employee { FirstName = "Vernon", LastName = "Pape", User_id = 1 });
             employee.Add(new Employee { FirstName = "Lynetta", LastName = "Richards", User_id = 2 });
             employee.Add(new Employee { FirstName = "LaNita", LastName = "Palmer", User_id = 3 });
+        }
+
+        /// <summary>
+        /// Gets a list of the first names of all users
+        /// </summary>
+        /// <param name="userId">The unique identifier for this person</param>
+        /// <param name="age">We want to know how old they are</param>
+        /// <returns>Alist of first names</returns>
+        [Route("api/Admin/GetFirstNames/{userId:int}/{age:int}")]
+        [HttpGet]
+        public List<string> GetFirstNames(int userId, int age)
+        {
+            List<string> output = new List<string>();
+
+            foreach (var e in employee)
+            {
+                output.Add(e.FirstName);
+            }
+
+            return output;
         }
 
         // GET: api/Admin
@@ -42,7 +65,13 @@ namespace Watson.Controllers
         // DELETE: api/Admin/5
         public void Delete(int id)
         {
-            
+            //Employee employee = db.Employees.Find(id);
+            //db.Employees.Remove(employee);
+            //db.SaveChanges();
+
+            //db.DeleteEmployeeAndDependents(id);
+
+            //return RedirectToAction("Index");
         }
     }
 }
